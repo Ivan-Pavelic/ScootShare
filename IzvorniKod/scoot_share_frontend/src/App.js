66790a8c1158634c4e3425415a62d7bc718d541b
AnamarijaJakoubek <anamarija.jakoubek@fer.hr>
2023-10-31 23:51:02 +0100

    adding login, rental, ad

--- IzvorniKod/scoot_share_frontend/src/App.js
+++ IzvorniKod/scoot_share_frontend/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import RegistrationPage from './pages/RegistrationPage';
+import LoginPage from './pages/LoginPage';
+import RentalPage from './pages/RentalPage';
+import AdPage from './pages/AdPage';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path="/register" element={<RegistrationPage/>}/> 
+        <Route path="/login" element={<LoginPage/>}/> 
+        <Route path="/rental" element={<RentalPage/>}/> 
+        <Route path="/ad" element={<AdPage/>}/> 
       </Routes>
     </BrowserRouter>
   );
