a98e5a67cf2d021f76525a9c299085eb2e0bbe9a
AnamarijaJakoubek <anamarija.jakoubek@fer.hr>
2023-10-31 23:49:19 +0100

    adding login button

--- IzvorniKod/scoot_share_frontend/src/components/NavigationComponent.js
+++ IzvorniKod/scoot_share_frontend/src/components/NavigationComponent.js
@@ -4,9 +4,13 @@ import { useNavigate } from 'react-router-dom';
 const NavigationComponent = (props) => {
     const navigate = useNavigate()
     const {displayRegisterButton} = {...props}
+    const {displayLoginButton} = {...props}
 
     function register() {
         navigate("/register")
+    }
+    function login() {
+        navigate("/login")
     }
 
     return (
@@ -14,8 +18,9 @@ const NavigationComponent = (props) => {
             <div className=''>
                 <p className='text-4xl text-white font-bold'>ScootShare</p>
             </div>
-            <div>
+            <div className='flex space-x-4'>
                 { displayRegisterButton && <button className='text-white bg-cyan-600 rounded-3xl py-3 px-8' onClick={register}>Registracija</button> }
+                { displayLoginButton && <button className='text-white bg-cyan-600 rounded-3xl py-3 px-8' onClick={login}>Prijava</button> }
             </div>
         </div>
     );
