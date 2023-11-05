4a24466b270fc77bbd952f2e3f854ff2ef3de062
Igor Sostarko <roquwat@gmail.com>
2023-11-05 13:14:28 +0100

    some fixes on controllers

--- IzvorniKod/scoot_share_backend/src/main/java/com/scootshare/base/controllers/HomepageController.java
+++ IzvorniKod/scoot_share_backend/src/main/java/com/scootshare/base/controllers/HomepageController.java
@@ -1,12 +1,15 @@
 package com.scootshare.base.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping("/")
 public class HomepageController {
 
-    @RequestMapping(value = "/")
+    @GetMapping("/")
     public String index() {
         return "index";
     }
--- IzvorniKod/scoot_share_backend/src/main/java/com/scootshare/base/controllers/RegistrationController.java
+++ IzvorniKod/scoot_share_backend/src/main/java/com/scootshare/base/controllers/RegistrationController.java
@@ -3,11 +3,13 @@ package com.scootshare.base.controllers;
 import com.scootshare.base.entities.User;
 import com.scootshare.base.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-@RestController
+@Controller
+@RequestMapping("/register")
 public class RegistrationController {
 
     private final UserService userService;
@@ -17,13 +19,15 @@ public class RegistrationController {
         this.userService = userService;
     }
 
-    @GetMapping("/register")
+    @GetMapping
     public String getRegistrationPage() {
         // add to return registration webpage
         return null;
     }
 
-    @PostMapping("/register")
+    //metoda u authenticationcontrolleru (?)
+    /*
+    @PostMapping
     public boolean processRegistrationRequest(@RequestBody User user
                                               //@RequestParam("idCard") MultipartFile idCard,
                                               //@RequestParam("certificateOfNoCriminalRecord") MultipartFile certificateOfNoCriminalRecord
@@ -35,5 +39,5 @@ public class RegistrationController {
         //String certificateUUID = fileDBService.store(certificateOfNoCriminalRecord).getId();
         userService.store(user);
         return true;
-    }
+    }*/
 }
