e60a976e9ec33a3f02084cb8f51f3c5186731945
Dino Babić <dinobabi3@gmail.com>
2023-11-15 22:15:19 +0100

    updating user

--- IzvorniKod/scoot_share_backend/src/main/java/com/scootshare/base/entities/User.java
+++ IzvorniKod/scoot_share_backend/src/main/java/com/scootshare/base/entities/User.java
@@ -14,7 +14,7 @@ import java.util.Set;
 public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
